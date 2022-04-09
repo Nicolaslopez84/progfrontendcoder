48e7db9aa14537a8eae1ebac9423688e0b3ebb90
Nicolas Lopez <nicolasrrlopez@gmail.com>
2022-04-09 15:28:18 -0300

    incorporando arrays

--- secciones/miRecibo.js
+++ secciones/miRecibo.js
@@ -7,18 +7,28 @@ class Sueldo{
         this.ausencias = parseInt (ausencias);
         this.sindicato = sindicato;
     }
-
+    calculohs(){
+        let valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
+        return valorExtras;
+    }
+    calculoBasico(){
+        let valorBruto = this.basico/30*(30-this.ausencias);
+        return valorBruto;
+    }
     sueldoNeto() {
         if (this.sindicato == "comercio") {
-            let neto = ( this.basico/30*(30-this.ausencias) + (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2) ) * 0.8
-            return neto;
+            let neto = ( this.calculoBasico() * 1.0833 + this.calculohs() ) * 0.80;
+            return alert(neto);
         }else{
-            let neto = ( this.basico/30*(30-this.ausencias) + (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2) ) * 0.83
-            return neto;
+            let neto = ( this.calculoBasico() + this.calculohs() ) * 0.83;
+            return alert(neto);
             }
     }
 }
 
 const total1 = new Sueldo (1000,0,10,1,"comercio");
-alert(total1.sueldoNeto());
+total1.sueldoNeto();
+
+
+
 
