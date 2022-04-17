0b23731d2c052ed3e73bf041a532ee8def9cf9d6
Nicolas Lopez <nicolasrrlopez@gmail.com>
2022-04-17 00:00:43 -0300

    modificando el dom 2

--- js/miRecibo.js
+++ js/miRecibo.js
@@ -2,13 +2,13 @@
 let basico = 0;
 let valorExtras = 0;
 let sueldoBruto = 0;
-const categoria = ["maest", "adm", "vend", "caj", "aux", "aux esp"];
+const categoria = ["Maestranza", "Administrativo", "Vendedor", "Cajero", "Auxiliar", "Auxiliar Esp"];
 const escalas = [79828.57, 80695.88, 80984.96, 80984.96, 80984.96, 81679.33];
+const sac = [];
 
 class Sueldo{
-    constructor(basico, basicoCom, hsLaV, hsFS, ausencias, sindicato){
+    constructor(basico, hsLaV, hsFS, ausencias, sindicato){
         this.basico = parseInt (basico);
-        this.basicoCom = basicoCom;  
         this.hsLaV = parseInt (hsLaV);
         this.hsFS = parseInt (hsFS);
         this.ausencias = parseInt (ausencias);
@@ -16,79 +16,93 @@ class Sueldo{
     };
 
     calculohs(){
-        if(this.sindicato == "comercio"){
-            valorExtras = (this.basicoCom/200*this.hsLaV*1.5) + (this.basicoCom/200*this.hsFS*2);
-            return valorExtras;
+        if(this.sindicato == "Comercio"){
+            valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
+            return parseInt(valorExtras);
         }else{
             valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
-            return valorExtras;
+            return parseInt(valorExtras);
         };
     };
 
     calculoBasico(){
-        if(this.sindicato == "comercio"){
-            sueldoBruto = this.basicoCom / 30 * (30-this.ausencias);
-            return sueldoBruto;
+        if(this.sindicato == "Comercio"){
+            sueldoBruto = ( this.basico / 30 * (30-this.ausencias) ) * 1.0833;
+            return parseInt(sueldoBruto);
         }else{
             sueldoBruto = this.basico / 30 * (30-this.ausencias);
-            return sueldoBruto;
+            return parseInt(sueldoBruto);
         };
     };
 
     sueldoNeto() {
-        if (this.sindicato == "comercio") {
-            let neto = ( this.calculoBasico() * 1.0833 + this.calculohs() + sac[0]/2 ) * 0.80;
+        if (this.sindicato == "Comercio") {
+            let neto = ( this.calculoBasico() + this.calculohs() ) * 0.80;
             return parseInt(neto);
         }else{
-            let neto = ( this.calculoBasico() + this.calculohs() + sac[0]/2 ) * 0.83;
+            let neto = ( this.calculoBasico() + this.calculohs() ) * 0.83;
             return parseInt(neto);
         };
     };
 };
 
-alert("Hola " + usuario + "! vamos a ayudarte a calcular tu sueldo del mes. Adelante!");
-let periodo = prompt("Que mes vas a liquidar?");
-let sindicato = prompt("Indicanos 'Comercio' si estas dentro de convenio o 'Fuera de convenio'");
-let hsLaV = prompt("Ingresa las horas extras que hiciste de lunes a viernes");
-let hsFS = prompt("Ahora ingresa las horas extras durante fines de semana o feriados");
-let ausencias = prompt("Excelente!!!Ahora decinos si tuviste ausencias injustificadas");    
-let cat = "";  
-const sac = [];
+document.getElementById("datosRecibo").addEventListener("submit", (e) =>{
+   
+    e.preventDefault();
 
-    if(periodo == "junio" || periodo == "diciembre"){   
-    for (let i = 1 ; i <= 6; i++){
+let periodo = document.getElementById("periodo").value
+let basico = parseInt(document.getElementById("Basico").value)
+let hsLaV = parseInt(document.getElementById("HsExtrasLaV").value);
+let hsFS = parseInt(document.getElementById("HsExtrasFS").value);
+let ausencias = parseInt(document.getElementById("ausencias").value);    
+let cat = document.getElementById("categorias").value;  
+
+
+
+/*    if(periodo == "junio" || periodo == "diciembre"){   
+    
+        for (let i = 1 ; i <= 6; i++){
         sac.push(prompt("Ingresa tus sueldos netos de enero a junio, o de julio a diciembre para conocer tu Aguinaldo"));
-    };
-    sac.sort((a, b) => b - a);
-    }
+    }   
+        sac.sort((a, b) => b - a)
 
-    if(sindicato == "comercio"){
-        cat = prompt("Sos del sindicato de Comercio, indicanos tu categoria: maest, adm, vend, caj, aux, aux esp");
+    };*/
 
-        while(categoria.includes(cat) == false){
-            cat = prompt("La categoria que ingresaste no es valida, volve a intertarlo")
-        };
 
-        const total1 = new Sueldo (0, escalas[categoria.indexOf(cat)], hsLaV, hsFS, ausencias, sindicato);
-        alert("Tu sueldo Neto sera de $" + total1.sueldoNeto());
-
+    if(document.getElementById("comercio").value == true){
+            console.log(true)
+        /*const total1 = new Sueldo (escalas[categoria.indexOf(cat)], hsLaV, hsFS, ausencias, "Comercio");
+        alert("Tu sueldo Neto sera de $" + total1.calculoBasico());
+        alert("Tu sueldo Neto sera de $" + total1.calculohs());
+        alert("Tu sueldo Neto sera de $" + total1.sueldoNeto())*/
+        
     }else{
-        basico = prompt("Ingresa tu sueldo basico");
-        const total1 = new Sueldo (basico, 0, hsLaV, hsFS, ausencias, sindicato);
-        alert("Tu sueldo Neto sera de $" + total1.sueldoNeto());
+        console.log(false)
+        /*const total1 = new Sueldo (basico, hsLaV, hsFS, ausencias, sindicato);
+        alert("Tu sueldo Neto sera de $" + total1.calculoBasico());
+        alert("Tu sueldo Neto sera de $" + total1.calculohs());
+        alert("Tu sueldo Neto sera de $" + total1.sueldoNeto())*/
+  
     };
 
+})
 
-let loquesea = document.getElementById("a")
-loquesea.inn
+/*function validacionSac(){
+    if (sac.length == 0){
+        return 0;
+    }else{
+        return sac[0];
+    }
+}*/
 
 
+let input1  = document.getElementById("FC");
+input1.addEventListener("input", (e) => {
 
+    document.getElementById("Basico").removeAttribute("disabled")
 
-
-
-
-
+  }
+)
 
 
 
