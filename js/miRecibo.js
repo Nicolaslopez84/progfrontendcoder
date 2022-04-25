6246363589034f6fb9a245e2b5015ee4cc7abd5e
Nicolas Lopez <nicolasrrlopez@gmail.com>
2022-04-25 01:05:04 -0300

    agregando JSON

--- js/miRecibo.js
+++ js/miRecibo.js
@@ -1,7 +1,3 @@
-
-let basico = 0;
-let valorExtras = 0;
-let sueldoBruto = 0;
 const categoria = ["Maestranza", "Administrativo", "Vendedor", "Cajero", "Auxiliar", "Auxiliar Esp"];
 const escalas = [79828.57, 80695.88, 80984.96, 80984.96, 80984.96, 81679.33];
 const sueldosCargados = [];
@@ -19,40 +15,51 @@ document.getElementById("sindicato").addEventListener("input", () => {
     }
 })
 
+//evento para mostrar sueldos guardados
+document.getElementById("btn__sueldos").addEventListener("click", () =>{
+   
+    let datosGuardados = document.getElementById("netos__guardados");
+    const almacenados = JSON.parse(localStorage.getItem(sessionStorage.getItem("usuario")))
+
+})
+
+
 //evento para el calculo del sueldo neto
-document.getElementById("datosRecibo").addEventListener("submit", (e) =>{
+document.getElementById("datos__recibo").addEventListener("submit", (e) =>{
    
     e.preventDefault();
 
-let periodo = document.getElementById("periodo").value;
-let sindicatoElegido = document.getElementById("sindicato").value;
-let basico = parseInt(document.getElementById("Basico").value)
-let hsLaV = parseInt(document.getElementById("HsExtrasLaV").value);
-let hsFS = parseInt(document.getElementById("HsExtrasFS").value);
-let ausencias = parseInt(document.getElementById("ausencias").value);    
-let cat = document.getElementById("categorias").value;  
-
-
+    let periodo = document.getElementById("periodo").value;
+    let sindicatoElegido = document.getElementById("sindicato").value;
+    let basico = parseInt(document.getElementById("Basico").value)
+    let hsLaV = parseInt(document.getElementById("HsExtrasLaV").value);
+    let hsFS = parseInt(document.getElementById("HsExtrasFS").value);
+    let ausencias = parseInt(document.getElementById("ausencias").value);    
+    let cat = document.getElementById("categorias").value;  
 
         if((basico > escalas[categoria.indexOf(cat)] && sindicatoElegido == "Comercio") || sindicatoElegido == "Fuera de Convenio"){
 
             const total1 = new Sueldo (basico, hsLaV, hsFS, ausencias, sindicatoElegido);
             
             alert("Tu sueldo Neto sera de $" + total1.sueldoNeto());
-            localStorage.setItem("periodo", periodo);
-            localStorage.setItem("Neto", total1.sueldoNeto());
+            sueldosCargados.push(new SueldosNetos(periodo, total1.sueldoNeto()));
+            for (const acum of sueldosCargados){
+                acum.guardarLocalStorage();
+                }
 
-        
         }else{
             
             const total1 = new Sueldo (escalas[categoria.indexOf(cat)], hsLaV, hsFS, ausencias, sindicatoElegido);
             alert("Tu sueldo Neto sera de $" + total1.sueldoNeto());
-            //sueldosCargados.push(new SueldosNetos(periodo, total1.sueldoNeto()));
+            sueldosCargados.push(new SueldosNetos(periodo, total1.sueldoNeto()));
+            for (const acum of sueldosCargados){
+                acum.guardarLocalStorage();
+            }
         }
-
-
 })
 
+
+//clase para el armado del JSON
 class SueldosNetos{
     constructor(periodo, neto)
     {
@@ -60,7 +67,10 @@ class SueldosNetos{
         this.neto = neto;
     }
 
+    guardarLocalStorage(){
+        localStorage.setItem(sessionStorage.getItem("usuario"), JSON.stringify(sueldosCargados));
 
+    }
 }
 
 
@@ -76,20 +86,20 @@ class Sueldo{
 
     calculohs(){
         if(this.sindicato == "Comercio"){
-            valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
+            let valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
             return parseInt(valorExtras);
         }else{
-            valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
+            let valorExtras = (this.basico/200*this.hsLaV*1.5) + (this.basico/200*this.hsFS*2);
             return parseInt(valorExtras);
         };
     };
 
     calculoBasico(){
         if(this.sindicato == "Comercio"){
-            sueldoBruto = ( this.basico / 30 * (30-this.ausencias) ) * 1.0833;
+            let sueldoBruto = ( this.basico / 30 * (30-this.ausencias) ) * 1.0833;
             return parseInt(sueldoBruto);
         }else{
-            sueldoBruto = this.basico / 30 * (30-this.ausencias);
+            let sueldoBruto = this.basico / 30 * (30-this.ausencias);
             return parseInt(sueldoBruto);
         };
     };
